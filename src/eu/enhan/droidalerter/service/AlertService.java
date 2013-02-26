4903eda331164a249a9d02f7ce2e19ab756d86df
Emmanuel Nhan <nhanmanu@gmail.com>
2013-02-26 15:44:31 +0100

    Alert Service shell for future use

---
+++ src/eu/enhan/droidalerter/service/AlertService.java
@@ -0,0 +1,14 @@
+package eu.enhan.droidalerter.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * @author Emmanuel Nhan
+ */
+public class AlertService extends Service {
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
